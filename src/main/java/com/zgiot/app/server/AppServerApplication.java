5c58638be58a531c7c87d3ec984b11d4a759c687
jiajunbo <jiajunbo>
2017-08-16 15:48:04 +0800

    init spring boot project structure

---
+++ src/main/java/com/zgiot/app/server/AppServerApplication.java
@@ -0,0 +1,12 @@
+package com.zgiot.app.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppServerApplication.class, args);
+    }
+}
