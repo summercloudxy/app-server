59d213882d3eb8a9883f3bd88ef208c7d6ee6b6a
jiajunbo <jiajunbo>
2017-08-17 11:27:21 +0800

    change db encoding

--- dbscript/smartfactory2-ddl.sql
+++ dbscript/smartfactory2-ddl.sql
@@ -10,10 +10,10 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `rel_label_metric`;
 CREATE TABLE `rel_label_metric` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `metric_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `metric_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `label_path` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for rel_thingtype_metric
@@ -21,10 +21,10 @@ CREATE TABLE `rel_label_metric` (
 DROP TABLE IF EXISTS `rel_thingtype_metric`;
 CREATE TABLE `rel_thingtype_metric` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `thingtype_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `metric_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `thingtype_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `metric_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_metric
@@ -32,16 +32,16 @@ CREATE TABLE `rel_thingtype_metric` (
 DROP TABLE IF EXISTS `tb_metric`;
 CREATE TABLE `tb_metric` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `metric_category_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `metric_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `metric_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `metric_type1_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `metric_type2_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `metric_type3_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `metric_category_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `metric_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `metric_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `metric_type1_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `metric_type2_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `metric_type3_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `valueType` varchar(3) DEFAULT NULL,
   `valueUnit` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_metrictype
@@ -49,10 +49,10 @@ CREATE TABLE `tb_metric` (
 DROP TABLE IF EXISTS `tb_metrictype`;
 CREATE TABLE `tb_metrictype` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `metrictype_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `metrictype_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `metrictype_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `metrictype_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_thing
@@ -61,14 +61,14 @@ DROP TABLE IF EXISTS `tb_thing`;
 CREATE TABLE `tb_thing` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_thing_id` int(11) DEFAULT NULL,
-  `thing_code` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `thing_category_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `thing_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `thing_type1_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `thing_type2_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `thing_type3_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `thing_code` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `thing_category_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `thing_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `thing_type1_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `thing_type2_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `thing_type3_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_thingtype
@@ -76,8 +76,8 @@ CREATE TABLE `tb_thing` (
 DROP TABLE IF EXISTS `tb_thingtype`;
 CREATE TABLE `tb_thingtype` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `thingtype_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `thingtype_code` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `thingtype_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `thingtype_code` varchar(3) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `parent_thingtype_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
