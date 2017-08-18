8bc5ec33829d92f4aac068d0940be71907e108e3
jiajunbo <jaycejia@gmail.com>
2017-08-18 13:26:05 +0800

    implement data service define and implement the data cache

---
+++ src/main/java/com/zgiot/app/server/service/cache/DataCacheImpl.java
@@ -0,0 +1,65 @@
+package com.zgiot.app.server.service.cache;
+
+import com.zgiot.common.pojo.DataModel;
+import com.zgiot.common.pojo.DataModelWrapper;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class DataCacheImpl implements DataCache {
+    private ConcurrentHashMap<String, DataModel> cache = new ConcurrentHashMap<>();
+
+    @Override
+    public DataModelWrapper getValue(String thingCode, String metricCode) {
+        DataModel data = cache.get(generateKey(thingCode, metricCode));
+        return data == null ? null : new DataModelWrapper(data);
+    }
+
+    @Override
+    public List<DataModelWrapper> findByThing(String thingCode) {
+        LinkedList<DataModelWrapper> result = new LinkedList<>();
+        cache.forEach((key, value) -> {
+            if (isThing(key, thingCode)) {
+                result.add(new DataModelWrapper(value));
+            }
+        });
+        return result;
+    }
+
+    @Override
+    public List<DataModelWrapper> findByMetric(String metricCode) {
+        LinkedList<DataModelWrapper> result = new LinkedList<>();
+        cache.forEach((key, value) -> {
+            if (isMetric(key, metricCode)) {
+                result.add(new DataModelWrapper(value));
+            }
+        });
+        return result;
+    }
+
+    @Override
+    public void updateValue(DataModel value) {
+        cache.put(generateKey(value.getThingCode(), value.getMetricCode()), value);
+    }
+
+    @Override
+    public boolean hasValue(String thingCode, String metricCode) {
+        return cache.containsKey(generateKey(thingCode, metricCode));
+    }
+
+    private String generateKey(String thingCode, String metricCode) {
+        return thingCode + "-" + metricCode;
+    }
+
+    private boolean isThing(String key, String thingCode) {
+        return key.startsWith(thingCode);
+    }
+
+    private boolean isMetric(String key, String metricCode) {
+        return key.endsWith(metricCode);
+    }
+
+}
--- src/main/java/com/zgiot/app/server/service/impl/DataServiceImpl.java
+++ src/main/java/com/zgiot/app/server/service/impl/DataServiceImpl.java
@@ -1,30 +1,37 @@
 package com.zgiot.app.server.service.impl;
 
+import com.zgiot.app.server.service.cache.DataCacheImpl;
 import com.zgiot.app.server.service.DataService;
 import com.zgiot.common.pojo.DataModel;
 import com.zgiot.common.pojo.DataModelWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class DataServiceImpl implements DataService{
+@Service
+public class DataServiceImpl implements DataService {
+    @Autowired
+    private DataCacheImpl dataCache;
+
     @Override
     public DataModelWrapper getData(String thingCode, String metricCode) {
-        return null;
+        return dataCache.getValue(thingCode, metricCode);
     }
 
     @Override
     public List<DataModelWrapper> findDataByThing(String thingCode) {
-        return null;
+        return dataCache.findByThing(thingCode);
     }
 
     @Override
     public List<DataModelWrapper> findDataByMetric(String metricCode) {
-        return null;
+        return dataCache.findByMetric(metricCode);
     }
 
     @Override
     public void updateCache(DataModel dataModel) {
-
+        dataCache.updateValue(dataModel);
     }
 
     @Override
