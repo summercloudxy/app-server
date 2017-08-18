1f752a9b1e3997854072279c4e3ca0c7c40e430d
jiajunbo <jaycejia@gmail.com>
2017-08-18 17:11:32 +0800

    add spring admin monitor

--- src/main/java/com/zgiot/app/server/AppServerApplication.java
+++ src/main/java/com/zgiot/app/server/AppServerApplication.java
@@ -1,9 +1,11 @@
 package com.zgiot.app.server;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class AppServerApplication {
 
     public static void main(String[] args) {
