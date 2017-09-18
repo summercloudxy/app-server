377c2fa7db339b0e9098a7e9cc4daa17cca8a46a
jeickey <jeickey@163.com>
2017-09-18 18:36:24 +0800

    fix format.

--- src/main/java/com/zgiot/app/server/module/generic/MetricController.java
+++ src/main/java/com/zgiot/app/server/module/generic/MetricController.java
@@ -2,7 +2,10 @@ package com.zgiot.app.server.module.generic;
 
 import com.zgiot.app.server.service.MetricService;
 import com.zgiot.common.pojo.MetricModel;
+import com.zgiot.common.restcontroller.ServerResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,10 @@ public class MetricController {
     private MetricService metricService;
 
     @GetMapping("/{metricCode}")
-    public MetricModel getMetric(@PathVariable String metricCode) {
-        return metricService.getMetric(metricCode);
+    public ResponseEntity<String> getMetric(@PathVariable String metricCode) {
+        MetricModel mm= metricService.getMetric(metricCode);
+        return new ResponseEntity<>(
+                ServerResponse.buildOkJson(mm)
+                , HttpStatus.OK);
     }
 }
