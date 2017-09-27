0a213d60d562ceaa69ede31c57b68b1dea348b71
jiajunbo <jaycejia@gmail.com>
2017-09-27 15:52:10 +0800

    add service

--- src/main/java/com/zgiot/app/server/mapper/TMLMapper.java
+++ src/main/java/com/zgiot/app/server/mapper/TMLMapper.java
@@ -2,10 +2,13 @@ package com.zgiot.app.server.mapper;
 
 import com.zgiot.common.pojo.MetricModel;
 import com.zgiot.common.pojo.ThingModel;
+import com.zgiot.common.pojo.ThingPropertyModel;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Set;
 
 @Mapper
 public interface TMLMapper {
@@ -15,5 +18,11 @@ public interface TMLMapper {
 
     @Select("SELECT * FROM `tb_thing` ")
     List<ThingModel> findAllThings();
+
+    @Select("SELECT * FROM `tb_thing_properties`")
+    List<ThingPropertyModel> findAllProperties();
+
+    @Select("SELECT metric_code FROM `rel_thing_metric_label` WHERE thing_code = #{thingCode}")
+    Set<String> findMetricsOfThing(String thingCode);
 
 }
