c8c7a354d61eeb1e162b44934b818b7807f05e1e
jeickey <jeickey@163.com>
2017-10-26 13:23:21 +0800

    Enable spring cache.

--- src/main/java/com/zgiot/app/server/AppServerApplication.java
+++ src/main/java/com/zgiot/app/server/AppServerApplication.java
@@ -2,8 +2,10 @@ package com.zgiot.app.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 //@EnableAdminServer
 public class AppServerApplication {
 
