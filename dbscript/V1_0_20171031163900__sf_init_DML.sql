2ff846641d26c9b954b5755131a97d02e8c821c7
summer cloud <469654418@qq.com>
2017-10-31 17:32:18 +0800

    启用所有plc点

--- dbscript/V1_0_20171031163900__sf_init_DML.sql
+++ dbscript/V1_0_20171031163900__sf_init_DML.sql
@@ -1,2 +1,3 @@
 UPDATE tb_metric SET  metric_type2_code='ZD' WHERE metric_name='震动';
 UPDATE tb_metric SET  metric_type2_code='TORQUE' WHERE metric_name='当前扭矩';
+UPDATE rel_thing_metric_label SET enabled = 1;
