be6ce89ba396b546581f0ee9dd73e1c6d4acaddf
summer cloud <469654418@qq.com>
2017-11-01 16:05:13 +0800

    修改部分故障点为保护点

--- dbscript/V1_0_20171031163900__sf_init_DML.sql
+++
@@ -1,3 +0,0 @@
-UPDATE tb_metric SET  metric_type2_code='ZD' WHERE metric_name='震动';
-UPDATE tb_metric SET  metric_type2_code='TORQUE' WHERE metric_name='当前扭矩';
-UPDATE rel_thing_metric_label SET enabled = 1;
