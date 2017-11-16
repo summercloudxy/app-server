e70eed6995fcd11939a7fd1b96a56de69e27c13a
summer cloud <469654418@qq.com>
2017-11-16 18:16:40 +0800

    修改文件上传接口

--- src/main/java/com/zgiot/app/server/module/generic/FileController.java
+++ src/main/java/com/zgiot/app/server/module/generic/FileController.java
@@ -8,17 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
+@RequestMapping("/file")
 public class FileController {
     @Autowired
     private FileService fileService;
 
-    @RequestMapping(name = "file/upload", method = RequestMethod.POST)
+    @PostMapping(value = "/upload")
     public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file, @RequestParam String module,
             @RequestParam int type, @RequestParam String userId) throws Exception {
         String uri = null;
