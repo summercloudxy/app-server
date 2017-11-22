179cccb55f555163822e3870aee6862e386c8560
wangfan <1577094401@qq.com>
2017-11-22 11:38:33 +0800

    sql下载bat脚本增加名称拆分，并增加文件不存在提示。

--- dbscript/compile_index2sql.bat
+++ dbscript/compile_index2sql.bat
@@ -3,15 +3,26 @@ set username=jenkins01
 set password=jenkins01
 set ip=192.168.5.28
 set warDir=/xg_smartfactory2/component/db
-echo open %ip%>ftp.txt  
-echo user %username% %password% >>ftp.txt  
-echo cd %warDir% >>ftp.txt  
+set postfix=-dev.sql
+set fileAddress=%cd%\
+set ftp=ftp.txt  
+set dir=dir.txt
+echo open %ip%>%ftp%
+echo user %username% %password% >>%ftp% 
+echo cd %warDir% >>%ftp%
+echo ls %warDir% %fileAddress%%dir% >>%ftp%
+echo bye >>%ftp%
+ftp -n -s:%ftp%
 for /f "delims=" %%i in ('dir /b /s /a-d *.index') do (
-echo %%i
-	for /f "delims=" %%j in (%%i) do (
-	echo %%j
-	echo get %%j >>ftp.txt  
+	for /f "delims=." %%j in (%%i) do (
+		findstr %%j%postfix% %dir% >nul &&(
+			echo %%j%postfix% is ok
+			echo get %%j%postfix% >>%ftp% 
+		)||(
+			echo %%j%postfix% is error
+			pause
+		)
 	)
 )
-echo bye >>ftp.txt  
-ftp -n -s:ftp.txt
+ftp -n -s:%ftp%
+echo bye >>%ftp%
