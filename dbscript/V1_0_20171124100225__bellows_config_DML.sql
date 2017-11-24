6677b71cf34e5e222aad54bad03fe958807d14f8
王伟 <wangwei@zhiguaniot.com>
2017-11-24 10:07:34 +0800

    添加鼓风DDL和DML

---
+++ dbscript/V1_0_20171124100225__bellows_config_DML.sql
@@ -0,0 +1,37 @@
+-- ----------------------------
+-- Records of tb_bellows_config
+-- ----------------------------
+INSERT INTO `tb_bellows_config` VALUES ('0', 'SYS', 'CP_INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('2', 'SYS', 'V_CHANGED', '0');
+INSERT INTO `tb_bellows_config` VALUES ('3', '2321.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('4', '2321.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('5', '2321.GF-2', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('6', '2321.GF-2', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('7', '2333.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('8', '2333.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('9', '2358.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('10', '2358.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('11', '2334.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('12', '2334.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('13', '2359.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('14', '2359.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('15', '2335.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('16', '2335.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('17', '2360.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('18', '2360.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('19', '2380.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('20', '2380.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('21', '2412.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('22', '2412.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('23', '2381.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('24', '2381.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('25', '2413.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('26', '2413.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('27', '2382.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('28', '2382.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('29', '2414.GF-1', 'TEAM', null);
+INSERT INTO `tb_bellows_config` VALUES ('30', '2414.GF-1', 'INTELLIGENT', '0');
+INSERT INTO `tb_bellows_config` VALUES ('31', 'SYS', 'V_MAX', '0');
+INSERT INTO `tb_bellows_config` VALUES ('32', 'SYS', 'V_WAIT_MIN', '0');
+INSERT INTO `tb_bellows_config` VALUES ('33', 'SYS', 'V_RUN_MIN', '0');
+INSERT INTO `tb_bellows_config` VALUES ('34', 'SYS', 'V_SPEED_LIMIT', '7');
