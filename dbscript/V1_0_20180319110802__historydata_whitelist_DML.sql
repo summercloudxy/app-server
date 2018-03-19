84d7dec254c80540011f4339c594d118aa500038
jiyongsheng <jys138210@163.com>
2018-03-19 12:31:12 +0800

    修改指标白名单

---
+++ dbscript/V1_0_20180319110802__historydata_whitelist_DML.sql
@@ -0,0 +1,179 @@
+--生产信息指标
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1301', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1301', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1301', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1301', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1301', 'CT_T', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1302', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1302', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1302', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1302', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1302', 'CT_T', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2301', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2301', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2301', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2301', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2301', 'CT_T', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2302', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2302', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2302', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2302', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2302', 'CT_T', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('702', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('702', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('702', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('702', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('702', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('702', 'CT_T', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('701', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('701', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('701', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('701', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('701', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('701', 'CT_T', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1552', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1552', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1552', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1552', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1552', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1552', 'CT_T', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2552', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2552', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2552', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2552', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2552', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2552', 'CT_T', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1554', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1554', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1554', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1554', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1554', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1554', 'CT_T', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1555', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1555', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1555', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1555', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1555', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1555', 'CT_T', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2554', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2554', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2554', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2554', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2554', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2554', 'CT_T', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2555', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2555', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2555', 'CT_D', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2555', 'CT_M', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2555', 'CT_Y', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2555', 'CT_T', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('Quit_SYS_1', 'Coal_8_Device', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('Quit_SYS_1', 'Coal_13_Device', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('Quit_SYS_2', 'Coal_8_Device', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('Quit_SYS_2', 'Coal_13_Device', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1143', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2143', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1144', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2144', 'COAL_CAP', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1143', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2143', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1144', 'CT_C', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2144', 'CT_C', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1553', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1553', 'CT_C', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2553', 'COAL_CAP', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2553', 'CT_C', 1);
+
+--功能卡片指标
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1601', 'CURRENT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1602', 'CURRENT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1603', 'CURRENT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2601', 'CURRENT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2602', 'CURRENT', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2492', 'PLATE_CNT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2493', 'PLATE_CNT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2494', 'PLATE_CNT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2495', 'PLATE_CNT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2496', 'PLATE_CNT', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2496A', 'PLATE_CNT', 1);	
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1510', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1510', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1510', 'FAULT', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1511', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1511', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1511', 'FAULT', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1512', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1512', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1512', 'FAULT', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1530', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1530', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1530', 'FAULT', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1531', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1531', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1531', 'FAULT', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1532', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1532', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('1532', 'FAULT', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2333', 'PRESS_CUR', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2334', 'PRESS_CUR', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2510', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2510', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2510', 'FAULT', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2511', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2511', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2511', 'FAULT', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2512', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2512', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2512', 'FAULT', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2530', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2530', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2530', 'FAULT', 1);
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2531', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2531', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2531', 'FAULT', 1);
+
+
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2532', 'RUN_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2532', 'LOAD_STATE', 1);
+INSERT INTO `smartfactory2`.`rel_historydata_whitelist`(`thing_code`, `metric_code`, `to_store`) VALUES ('2532', 'FAULT', 1);
