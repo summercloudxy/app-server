28d51bad7eae483448d59c29ade59519411e53ae
jiyongsheng <jys138210@163.com>
2018-03-29 13:49:57 +0800

    增加白名单汇总分钟信号数据

---
+++ dbscript/V1_0_20180328151545__update_rel_historydata_whitelist_DML.sql
@@ -0,0 +1,1 @@
+UPDATE rel_historydata_whitelist set minute_summary =1 where metric_code='COAL_CAP' and thing_code in('1301','1302','2302','2301','701','702','1552','2552','1554','1555','2554','2555');
