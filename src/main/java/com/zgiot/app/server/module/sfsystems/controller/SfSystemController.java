f17a93a3ac91f0c52a1cf6eb9848f22c3c19f016
e <123456@qq.com>
2018-04-02 10:03:12 +0800

    '系统设备'

--- src/main/java/com/zgiot/app/server/module/sfsystems/controller/SfSystemController.java
+++ src/main/java/com/zgiot/app/server/module/sfsystems/controller/SfSystemController.java
@@ -1,10 +1,44 @@
 package com.zgiot.app.server.module.sfsystems.controller;
 
+import com.zgiot.app.server.module.equipments.pojo.RelThingSystem;
+import com.zgiot.app.server.module.equipments.service.RelThingSystemService;
 import com.zgiot.common.constants.GlobalConstants;
+import com.zgiot.common.restcontroller.ServerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = GlobalConstants.API  + GlobalConstants.API_VERSION + "/sfsystems")
 public class SfSystemController {
+
+    @Autowired
+    private RelThingSystemService relThingSystemService;
+
+    /**
+     * 添加系统设备
+     * @param relThingSystem
+     * @return
+     */
+    @RequestMapping(value = "/relThingSystem/add", method = RequestMethod.POST)
+    public ResponseEntity<String> addBuilding(RelThingSystem relThingSystem) {
+        relThingSystemService.addRelThingSystem(relThingSystem);
+        return new ResponseEntity<>(ServerResponse.buildOkJson(null), HttpStatus.OK);
+    }
+
+    /**
+     * 系统设备删除
+     * @param thing_code
+     * @return
+     */
+    @RequestMapping(value = "/relThingSystem/delete/{thing_code}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> delBuilding(@PathVariable("thing_code") String thing_code) {
+        relThingSystemService.deleteRelThingSystemByThingCode(thing_code);
+        return new ResponseEntity<>(ServerResponse.buildOkJson(null), HttpStatus.OK);
+    }
+
 }
