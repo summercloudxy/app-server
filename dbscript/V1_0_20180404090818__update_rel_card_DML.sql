61bcb304acca9c5795632e4518054a5690377532
xiayun <469654418@qq.com>
2018-04-04 09:33:58 +0800

    修改卡片错误配置

---
+++ dbscript/V1_0_20180404090818__update_rel_card_DML.sql
@@ -0,0 +1,3 @@
+UPDATE `rel_qualityandquantity_area_card` SET `id`=NULL, `area_id`='4', `card_id`='15', `sequence`='1', `description`='概况产品及质量数据二期', `area_title`='产品和质量数据' WHERE  (`area_id`='4') AND (`card_id`='10') AND (`sequence`='1') AND (`description`='概况产品及质量数据二期') AND (`area_title`='产品和质量数据') LIMIT 1;
+
+UPDATE `rel_qualityandquantity_area_card` SET `id`=NULL, `area_id`='8', `card_id`='12', `sequence`='1', `description`='原煤入洗一期', `area_title`='一期' WHERE  (`area_id`='8') AND (`card_id`='5') AND (`sequence`='1') AND (`description`='原煤入洗一期') AND (`area_title`='一期') LIMIT 1;
