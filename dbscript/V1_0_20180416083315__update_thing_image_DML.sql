3c5ae19242343016538d41fb361cf4046f99d3bf
xiayun <469654418@qq.com>
2018-04-16 09:09:28 +0800

    修改设备图片名称

--- dbscript/V1_0_20180416083315__update_thing_image_DML.sql
+++
@@ -1,4 +0,0 @@
-update tb_thing_properties set prop_value = 'zhenggeimeiyou.png' where prop_value = 'zhengeimeiyou.png';
-update tb_thing_properties set prop_value = 'zhenggeimeizuo.png' where prop_value = 'zhengeimeizuo.png';
-update tb_thing_properties set prop_value = 'ganqiancaozuo.png' where prop_value = 'ganqiancaozou.png';
-update tb_thing_properties set prop_value = 'jingqiancaozuo.png' where prop_value = 'jingqiancaozou.png';
