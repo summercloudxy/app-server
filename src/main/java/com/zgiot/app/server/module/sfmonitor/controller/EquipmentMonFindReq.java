53c87f4e672d114933497b58efdb0a601d41ad44
lulansong <lulansong@126.com>
2018-04-24 19:24:48 +0800

    监控bug修改

--- src/main/java/com/zgiot/app/server/module/sfmonitor/controller/EquipmentMonFindReq.java
+++ src/main/java/com/zgiot/app/server/module/sfmonitor/controller/EquipmentMonFindReq.java
@@ -3,6 +3,8 @@ package com.zgiot.app.server.module.sfmonitor.controller;
 public class EquipmentMonFindReq {
     private String configProgress;
     private String thingCode;
+    private int pageNum;
+    private int pageSize;
 
     public String getConfigProgress() {
         return configProgress;
@@ -12,11 +14,27 @@ public class EquipmentMonFindReq {
         return thingCode;
     }
 
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
     public void setConfigProgress(String configProgress) {
         this.configProgress = configProgress;
     }
 
     public void setThingCode(String thingCode) {
         this.thingCode = thingCode;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
     }
 }
