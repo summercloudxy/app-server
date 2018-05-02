11f88161c86cda7d2df4717607d1527af2ff065a
wang.lei <423223340@qq.com>
2018-05-02 09:09:51 +0800

    '修改用户权限配置'

--- src/main/java/com/zgiot/app/server/module/sfmonitor/mapper/RelSFMonMetrictypeMetricMapper.java
+++ src/main/java/com/zgiot/app/server/module/sfmonitor/mapper/RelSFMonMetrictypeMetricMapper.java
@@ -2,13 +2,14 @@ package com.zgiot.app.server.module.sfmonitor.mapper;
 
 import com.zgiot.app.server.module.sfmonitor.pojo.RelSFMonMetrictypeMetric;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface RelSFMonMetrictypeMetricMapper {
 
-    List<RelSFMonMetrictypeMetric> getRelSFMonMetrictypeMetricByThingCode(@Param("thingCodeList") List<String> thingCodeList);
+    @Select("select * from rel_sfmon_metrictype_metric")
+    List<RelSFMonMetrictypeMetric> getAllRelSFMonMetrictypeMetric();
 
 }
