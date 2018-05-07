f06038c38510b6cb8ce91338a818b66e10df2ae5
jiyongsheng <jys138210@163.com>
2018-05-07 12:58:42 +0800

    智能加介增加数据库脚本

---
+++ dbscript/V1_0_20180507113402__create_tb_medium_config__DDL.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `tb_medium_compounding_config`  (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `medium_pool_code` int(11) NULL DEFAULT NULL COMMENT '介质池Code',
+  `medium_compounding_level` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '配介液位',
+  `low_level` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '最低液位',
+  `medium_compounding_state` tinyint(1) NULL DEFAULT NULL COMMENT '配介状态',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+
+
+CREATE TABLE `tb_medium_dosing_config`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `system` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '一期二期',
+  `system_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '系统名称',
+  `separatingsystem_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '分选系统名称',
+  `combinedbucket_code` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '合介桶编号',
+  `mediumdosingsystem_id` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '所属加介系统id',
+  `mediumdosingsystem_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '所属加介系统名称',
+  `mediumdosingvalue_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '加介阀门',
+  `changevalue_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '一二期切换阀门',
+  `mediumdosingpump_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '加介泵',
+  `mediumpool_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '介质池',
+  `watersupplementvalve_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '补水阀',
+  `blowervalve_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '鼓风阀',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
---
+++ dbscript/V1_0_20180507113404__add_tb_thing__DML.sql
@@ -0,0 +1,51 @@
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4704, NULL, '1540.FM-1', '一期切换阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4705, NULL, '1540.FM-2', '二期切换阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4706, NULL, '1547.FM-1', '1#介质池补水阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4707, NULL, '1547.FM-2', '1#介质池1鼓风阀', 'VALUE', 'QF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4708, NULL, '1541.FM-1', '一期切换阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4709, NULL, '1541.FM-2', '二期切换阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4710, NULL, '1548.FM-1', '2#介质池补水阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4711, NULL, '1548.FM-2', '2#介质池鼓风阀', 'VALUE', 'QF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4712, NULL, '1542.FM-1', '一期切换阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4713, NULL, '1542.FM-2', '二期切换阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4714, NULL, '1549.FM-1', '3#介质池补水阀', 'VALUE', 'DZF', NULL, NULL);
+INSERT INTO `tb_thing`(`id`, `parent_thing_id`, `thing_code`, `thing_name`, `thing_type1_code`, `thing_type2_code`, `thing_type3_code`, `thing_shortname`) VALUES (4715, NULL, '1549.FM-2', '3#介质池鼓风阀', 'VALUE', 'QF', NULL, NULL);
+
+
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12015, '1547', 'IMAGE_NAME', 'shuixiang.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12016, '1548', 'IMAGE_NAME', 'shuixiang.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12017, '1549', 'IMAGE_NAME', 'shuixiang.png', 'DISP_PROP');
+
+
+
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12018, '1541.JJ-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12019, '1541.JJ-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12020, '1541.JJ-3', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12021, '1542.JJ-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12022, '1542.JJ-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12023, '1542.JJ-3', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12024, '2541.JJ-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12025, '2541.JJ-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12026, '2541.JJ-3', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12027, '2542.JJ-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12028, '2542.JJ-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12029, '2542.JJ-3', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12030, '1540.FM-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12031, '1540.FM-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12032, '1547.FM-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12033, '1547.FM-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12034, '1541.FM-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12035, '1541.FM-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12036, '1548.FM-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12037, '1548.FM-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12038, '1542.FM-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12039, '1542.FM-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12040, '1549.FM-1', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12041, '1549.FM-2', 'IMAGE_NAME', 'famen.png', 'DISP_PROP');
+
+
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12042, '1540', 'IMAGE_NAME', 'jiezhibeng.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12043, '1541', 'IMAGE_NAME', 'jiezhibeng.png', 'DISP_PROP');
+INSERT INTO `tb_thing_properties`(`id`, `thing_code`, `prop_key`, `prop_value`, `prop_type`) VALUES (12044, '1542', 'IMAGE_NAME', 'jiezhibeng.png', 'DISP_PROP');
+
+
