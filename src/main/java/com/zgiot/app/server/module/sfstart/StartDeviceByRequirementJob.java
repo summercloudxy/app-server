8e73e80477941b2c2deb4e151637a250a0910547
jiyongsheng <jys138210@163.com>
2018-05-23 17:10:08 +0800

    修改文件名称

--- src/main/java/com/zgiot/app/server/module/sfstart/StartDeviceByRequirementJob.java
+++ src/main/java/com/zgiot/app/server/module/sfstart/StartDeviceByRequirementJob.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
-public class SartDeviceByRequirementJob implements Job {
+public class StartDeviceByRequirementJob implements Job {
 
     private final Logger logger = LoggerFactory.getLogger(AlertFaultJob.class);
 
