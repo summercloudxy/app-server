bc2296668377efad0b6eca914baed2c8d688bffb
wang.lei <423223340@qq.com>
2018-05-24 15:39:26 +0800

    '用户模块因表结构变化的修改'

--- src/main/java/com/zgiot/app/server/module/userfavor/controller/PadModuleController.java
+++ src/main/java/com/zgiot/app/server/module/userfavor/controller/PadModuleController.java
@@ -1,6 +1,7 @@
 package com.zgiot.app.server.module.userfavor.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.zgiot.app.server.module.sfsubsc.constants.SFSubscConstant;
 import com.zgiot.app.server.module.userfavor.mapper.PadModuleMapper;
 import com.zgiot.app.server.module.userfavor.pojo.UserFavor;
 import com.zgiot.app.server.module.userfavor.pojo.PadModule;
@@ -30,7 +31,7 @@ public class PadModuleController {
 
     @GetMapping("/userFavor/user/{userUuid}")
     public ResponseEntity<String> getPadModule(@PathVariable String userUuid) {
-        List<UserFavor> userFavorList = padModuleMapper.getUserFavor(userUuid);
+        List<UserFavor> userFavorList = padModuleMapper.getUserFavor(userUuid, Long.parseLong(SFSubscConstant.PAD_CLIENT_ID));
         return new ResponseEntity(ServerResponse.buildOkJson(userFavorList), HttpStatus.OK);
     }
 
@@ -38,16 +39,17 @@ public class PadModuleController {
     public ResponseEntity<String> addPadModule(@RequestBody String bodyStr) {
         UserFavorReq userFavorReq = JSON.parseObject(bodyStr, UserFavorReq.class);
         String userUuid = userFavorReq.getUserUuid();
+        long client_id = Long.parseLong(SFSubscConstant.PAD_CLIENT_ID);
         if (!StringUtils.isBlank(userUuid) && userFavorReq.getModuleIds().size() > 0) {
-            Float sort = padModuleMapper.getMaxSort(userUuid);
-            List<UserFavor> userFavors = padModuleMapper.getUserFavor(userUuid);
+            Float sort = padModuleMapper.getMaxSort(userUuid, client_id);
+            List<UserFavor> userFavors = padModuleMapper.getUserFavor(userUuid, client_id);
             if (sort == null) {
                 sort = 0f;
             }
-            padModuleMapper.deleteUserFavor(userUuid);
+            padModuleMapper.deleteUserFavor(userUuid, client_id);
             addUserFavor(userFavorReq, userUuid, sort, userFavors);
         } else {
-            padModuleMapper.deleteUserFavor(userUuid);
+            padModuleMapper.deleteUserFavor(userUuid, client_id);
         }
 
         return new ResponseEntity(ServerResponse.buildOkJson(null), HttpStatus.OK);
@@ -56,10 +58,11 @@ public class PadModuleController {
     private void addUserFavor(UserFavorReq userFavorReq, String userUuid, Float sort, List<UserFavor> userFavors) {
         for (long padModuleId : userFavorReq.getModuleIds()) {
             UserFavor userFavor = new UserFavor();
+            userFavor.setClientId(Long.parseLong(SFSubscConstant.PAD_CLIENT_ID));
             userFavor.setUserUuid(userUuid);
             if (userFavors.size() > 0) {
                 for (UserFavor userFavorTemp : userFavors) {
-                    if (userFavorTemp.getUserUuid().equals(userUuid) && (userFavorTemp.getPadModuleId() == padModuleId)) {
+                    if (userFavorTemp.getUserUuid().equals(userUuid) && (userFavorTemp.getModuleId() == padModuleId)) {
                         userFavor.setSort(userFavorTemp.getSort());
                         break;
                     }
@@ -68,8 +71,8 @@ public class PadModuleController {
             if (userFavor.getSort() == 0) {
                 userFavor.setSort(++sort);
             }
-            userFavor.setPadModuleId(padModuleId);
-            UserFavor favor = padModuleMapper.getUserFavorById(userUuid, padModuleId);
+            userFavor.setModuleId(padModuleId);
+            UserFavor favor = padModuleMapper.getUserFavorById(userUuid, padModuleId, Long.parseLong(SFSubscConstant.PAD_CLIENT_ID));
             if (favor == null) {
                 padModuleMapper.addUserFavor(userFavor);
             }
